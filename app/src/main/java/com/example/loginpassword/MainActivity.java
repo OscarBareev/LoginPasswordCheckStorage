503edef4de3ed784329b9e5cf143a0d7676b9fb6
Oscar Bareev <oscar.bareev@gmail.com>
2020-06-27 15:10:45 +0300

    Homework first try

--- app/src/main/java/com/example/loginpassword/MainActivity.java
+++ app/src/main/java/com/example/loginpassword/MainActivity.java
@@ -1,30 +1,42 @@
 package com.example.loginpassword;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.Scanner;
 
 public class MainActivity extends AppCompatActivity {
 
-    private final String LOGIN_FILE_NAME = "login.txt";
-    private final String PASSWORD_FILE_NAME = "password.txt";
+
+    private final String FILE_NAME = "file.txt";
+    private final String PREF_KEY = "prefKey";
 
     EditText loginForm;
     EditText passwordForm;
     Button loginBtn;
     Button registrationBtn;
+    CheckBox checkBox;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,6 +53,31 @@ public class MainActivity extends AppCompatActivity {
         passwordForm = findViewById(R.id.editTextPassword);
         loginBtn = findViewById(R.id.LoginBtn);
         registrationBtn = findViewById(R.id.RegistrationBtn);
+        checkBox = findViewById(R.id.checkBox);
+
+        SharedPreferences preferences = getSharedPreferences("MyPref", MODE_PRIVATE);
+        final SharedPreferences.Editor checkEditor = preferences.edit();
+
+
+        if (preferences.getBoolean(PREF_KEY, false)) {
+            checkBox.setChecked(true);
+        } else {
+            checkBox.setChecked(false);
+        }
+
+
+        checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if (checkBox.isChecked()) {
+                    checkEditor.putBoolean(PREF_KEY, true);
+                } else {
+                    checkEditor.putBoolean(PREF_KEY, false);
+                }
+                checkEditor.apply();
+            }
+        });
+
 
         loginBtn.setOnClickListener(myButtonClickListener);
         registrationBtn.setOnClickListener(myButtonClickListener);
@@ -48,6 +85,7 @@ public class MainActivity extends AppCompatActivity {
 
 
     View.OnClickListener myButtonClickListener = new View.OnClickListener() {
+        @RequiresApi(api = Build.VERSION_CODES.KITKAT)
         @Override
         public void onClick(View view) {
 
@@ -63,22 +101,19 @@ public class MainActivity extends AppCompatActivity {
                 switch (view.getId()) {
                     case R.id.LoginBtn:
 
-                        if (readFile(LOGIN_FILE_NAME, loginContent)
-                                && readFile(PASSWORD_FILE_NAME, passwordContent)) {
-                            Toast.makeText(MainActivity.this,
-                                    R.string.AllIsOK, Toast.LENGTH_SHORT).show();
+                        if (checkBox.isChecked()) {
+                            readExternalFile(loginContent + passwordContent);
                         } else {
-                            Toast.makeText(MainActivity.this,
-                                    R.string.wrongData, Toast.LENGTH_SHORT).show();
+                            readFile(loginContent + passwordContent);
                         }
                         break;
 
                     case R.id.RegistrationBtn:
 
-                        if (writeFile(LOGIN_FILE_NAME, loginContent)
-                                && writeFile(PASSWORD_FILE_NAME, passwordContent)) {
-                            Toast.makeText(MainActivity.this,
-                                    R.string.regOk, Toast.LENGTH_SHORT).show();
+                        if (checkBox.isChecked()) {
+                            writeExternalFile(loginContent + passwordContent);
+                        } else {
+                            writeFile(loginContent + "\n" + passwordContent);
                         }
                         break;
                 }
@@ -87,55 +122,103 @@ public class MainActivity extends AppCompatActivity {
     };
 
 
-    private boolean writeFile(String fileName, String content) {
+    private void writeFile(String content) {
         try {
 
-            FileOutputStream outputStream = openFileOutput(fileName, MODE_PRIVATE);
+            FileOutputStream outputStream = openFileOutput(FILE_NAME, MODE_PRIVATE);
             OutputStreamWriter outputStreamWriter = new OutputStreamWriter(outputStream);
             BufferedWriter bufferedWriter = new BufferedWriter(outputStreamWriter);
             bufferedWriter.write(content);
             bufferedWriter.close();
-            return true;
+            Toast.makeText(MainActivity.this,
+                    R.string.regOk, Toast.LENGTH_SHORT).show();
 
         } catch (FileNotFoundException e) {
             e.printStackTrace();
-            return false;
+
 
         } catch (IOException e) {
             e.printStackTrace();
-            return false;
         }
     }
 
-    private boolean readFile(String fileName, String content) {
 
-        boolean isEquals = false;
+    private void readFile(String content) {
 
         try {
-            InputStreamReader inputStreamReader = new InputStreamReader(openFileInput(fileName));
+            InputStreamReader inputStreamReader = new InputStreamReader(openFileInput(FILE_NAME));
             BufferedReader reader = new BufferedReader(inputStreamReader);
             String line = reader.readLine();
             StringBuilder sb = new StringBuilder();
 
             while (line != null) {
                 sb.append(line);
+                Log.d("Tag", line);
                 line = reader.readLine();
             }
 
             line = sb.toString();
 
-
             if (line.equals(content)) {
-                isEquals = true;
+                Toast.makeText(MainActivity.this,
+                        R.string.AllIsOK, Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(MainActivity.this,
+                        R.string.wrongData, Toast.LENGTH_SHORT).show();
             }
 
         } catch (FileNotFoundException e) {
             e.printStackTrace();
+            Toast.makeText(MainActivity.this,
+                    R.string.wrongData, Toast.LENGTH_SHORT).show();
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
 
-        return isEquals;
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    private void writeExternalFile(String content) {
+        File dataFile = new File(getApplicationContext().getExternalFilesDir(null), FILE_NAME);
+        try (FileWriter dataWriter = new FileWriter(dataFile, true)) {
+            dataWriter.append(content);
+            Toast.makeText(MainActivity.this,
+                    R.string.regOk, Toast.LENGTH_SHORT).show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    private void readExternalFile(String content) {
+        File dataFile = new File(getApplicationContext().getExternalFilesDir(null), FILE_NAME);
+
+        if (dataFile.exists() && dataFile.isFile()) {
+
+            try (FileReader dataReader = new FileReader(dataFile)) {
+
+                Scanner scanner = new Scanner(dataReader);
+
+                if (scanner.hasNext()) {
+                    String line = scanner.nextLine();
+                    if (line.equals(content)) {
+                        Toast.makeText(MainActivity.this,
+                                R.string.AllIsOK, Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(MainActivity.this,
+                                R.string.wrongData, Toast.LENGTH_SHORT).show();
+                    }
+                }
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            Toast.makeText(MainActivity.this,
+                    R.string.wrongData, Toast.LENGTH_SHORT).show();
+        }
     }
 }
 
